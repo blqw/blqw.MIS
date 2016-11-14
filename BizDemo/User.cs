30cf4e46503403f13e212eac05da58fc97e4e626
周子鉴 <blqw@live.com>
2016-11-15 01:33:27 +0800

    构思

---
+++ BizDemo/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using blqw.SIF;
+
+namespace BizDemo
+{
+    [SI(Container = "iis", SettingString = "Route={class}/{action}")]
+    [SI(Container = "wcf", SettingString = "Por")]
+    public class User
+    {
+        [SI(Container = "iis", SettingString = "Route={id};HttpMethod=get")]
+        public object Get(int id)
+        {
+            return new { id = 1, created_time = DateTime.Now };
+        }
+    }
+}
---
+++ blqw.SIF/SIAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace blqw.SIF
+{
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    public class SIAttribute : Attribute
+    {
+        public string Container { get; set; }
+        public string SettingString { get; set; }
+
+        private Dictionary<string, string> _values = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+        public string this[string name]
+        {
+            get
+            {
+                string value;
+                return _values.TryGetValue(name, out value) ? value : null;
+            }
+        }
+    }
+}
